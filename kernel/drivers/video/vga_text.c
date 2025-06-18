78598c0f7d1b2feeb25ae0201e1982d7fd699098
Sumukh Prasad <sumukhprasad@icloud.com>
2025-06-18 16:31:50 +0530

    optimised vga text driver

--- kernel/drivers/video/vga_text.c
+++ kernel/drivers/video/vga_text.c
@@ -13,45 +13,40 @@
 char attribute_byte = GRAY_ON_BLACK;
 
 void print_char(char character, int col, int row) {
-	unsigned char * vid_mem = (unsigned char * ) VGA_ADDRESS;
+	unsigned char *vid_mem = (unsigned char *)VGA_ADDRESS;
 	int offset;
 
 	if (col >= 0 && row >= 0)
 		offset = get_scr_offset(col, row);
 	else
 		offset = get_cursor();
-	
-	if ('\n' == character) {
-		offset = get_scr_offset(MAX_COLS - 1, offset / (2 * MAX_COLS));
+
+	if (character == '\n') {
+		int rows = offset / (2 * MAX_COLS);
+		offset = get_scr_offset(0, rows + 1);
 	} else {
 		vid_mem[offset] = character;
 		vid_mem[offset + 1] = attribute_byte;
+		offset += 2;
 	}
 
-	offset += 2;
-	
 	offset = handle_scrolling(offset);
-
 	set_cursor(offset);
 }
 
 int get_scr_offset(int col, int row) {
-	int offset = (row * MAX_COLS + col) * 2;
-
-	return offset;
+	return (row * MAX_COLS + col) * 2;
 }
 
 int get_cursor(void) {
-	int offset = 0;
+	int offset;
 
 	pbout(REG_SCR_CTRL, 0x0E);
 	offset = pbin(REG_SCR_DATA) << 8;
 	pbout(REG_SCR_CTRL, 0x0F);
 	offset |= pbin(REG_SCR_DATA);
 
-	offset *= 2;
-
-	return offset;
+	return offset * 2;
 }
 
 void set_cursor(int offset) {
@@ -60,64 +55,54 @@ void set_cursor(int offset) {
 	pbout(REG_SCR_CTRL, 0x0E);
 	pbout(REG_SCR_DATA, (unsigned char)(offset >> 8));
 	pbout(REG_SCR_CTRL, 0x0F);
-	pbout(REG_SCR_DATA, (unsigned char) offset);
+	pbout(REG_SCR_DATA, (unsigned char)offset);
 }
 
-void print_at(char * str, int col, int row) {
-	int i;
-	bool coordinates = false;
-
-	if (col >= 0 && row >= 0) {
+void print_at(char *str, int col, int row) {
+	if (col >= 0 && row >= 0)
 		set_cursor(get_scr_offset(col, row));
-		coordinates = true;
-	}
 
-	if (!coordinates)
-		for (i = 0; str[i] != '\0'; ++i)
-			print_char(str[i], col, row);
-	else
-		for (i = 0; str[i] != '\0'; ++i, ++col)
-			print_char(str[i], col, row);
-
+	while (*str)
+		print_char(*str++, -1, -1);
 }
 
 void clear_screen(void) {
-	for (int row = 0; row < MAX_ROWS; ++row) {
-		for (int col = 0; col < MAX_COLS; ++col) {
-			print_char(' ', col, row);
-		}
+	unsigned char *vid_mem = (unsigned char *)VGA_ADDRESS;
+
+	for (int i = 0; i < MAX_ROWS * MAX_COLS * 2; i += 2) {
+		vid_mem[i] = ' ';
+		vid_mem[i + 1] = attribute_byte;
 	}
+
 	set_cursor_to_origin();
 }
 
 void set_cursor_to_origin(void) {
-	set_cursor(get_scr_offset(0, 0));
+	set_cursor(0);
 }
 
 int handle_scrolling(int cursor_offset) {
 	if (cursor_offset < MAX_ROWS * MAX_COLS * 2)
 		return cursor_offset;
 
-	int i;
-	for (i = 1; i < MAX_ROWS; ++i) {
-		mem_cpy((char * )(get_scr_offset(0, i - 1) + VGA_ADDRESS),
-			(char * )(get_scr_offset(0, i) + VGA_ADDRESS),
-			MAX_COLS * 2);
+	for (int row = 1; row < MAX_ROWS; ++row) {
+		mem_cpy((char *)(get_scr_offset(0, row - 1) + VGA_ADDRESS),
+				(char *)(get_scr_offset(0, row) + VGA_ADDRESS),
+				MAX_COLS * 2);
 	}
 
-	char * lst_ln = (char * )(get_scr_offset(0, MAX_ROWS - 1) + VGA_ADDRESS);
-	for (i = 0; i < MAX_COLS*2; i += 2)
-		lst_ln[i] = ' ';
+	unsigned char *last_line = (unsigned char *)(get_scr_offset(0, MAX_ROWS - 1) + VGA_ADDRESS);
+	for (int i = 0; i < MAX_COLS * 2; i += 2) {
+		last_line[i] = ' ';
+		last_line[i + 1] = attribute_byte;
+	}
 
-	cursor_offset -= 2 * MAX_COLS;
-
-	return cursor_offset;
+	return cursor_offset - (2 * MAX_COLS);
 }
 
 char get_char_at(int row, int col) {
-	unsigned char * vid_mem = (unsigned char * ) VGA_ADDRESS;
-	int offset = get_scr_offset(row, col);
-
+	unsigned char *vid_mem = (unsigned char *)VGA_ADDRESS;
+	int offset = get_scr_offset(col, row);
 	return vid_mem[offset];
 }
 
@@ -133,23 +118,22 @@ void hide_cursor(void) {
 	pwout(REG_SCR_CTRL, HIDE_CURSOR);
 }
 
-void vga_print(const char* str) {
-	while (*str) {
+void vga_print(const char *str) {
+	while (*str)
 		print_char(*str++, -1, -1);
-	}
 }
 
-void vga_println(const char* str) {
+void vga_println(const char *str) {
 	vga_print(str);
 	print_char('\n', -1, -1);
 }
 
 void vga_printchar(char character) {
-	if (character == NULL)
+	if (character == '\0')
 		return;
 	print_char(character, -1, -1);
 }
 
 void vga_printdec(int i) {
-	 vga_print(itoa(i, 10));
+	vga_print(itoa(i, 10));
 }
