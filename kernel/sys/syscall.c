96e7df515b5bd91cae91d507b9c820fc3eb39876
Sumukh Prasad <sumukhprasad@icloud.com>
2025-07-03 16:21:07 +0530

    added syscall definitions

--- kernel/sys/syscall.c
+++ kernel/sys/syscall.c
@@ -1,11 +1,24 @@
-// syscall.c
 #include "syscall.h"
 #include "../klib/console/console.h"
 #include "../cpu/user_switch.h"
 #include "../utils.h"
+#include "../klib/uptime/uptime.h"
 
 __attribute__((noreturn))
 void syscall_exit_trampoline(uint32_t esp, void* ret_addr) {
+	console_print("restoring kernel ESP, value was 0x");
+	console_print_hex(kernel_ctx.esp);
+	console_print(".");
+	console_putc('\n');
+	
+	console_print("return address is 0x");
+	console_print_hex((uint32_t)kernel_ctx.ret_addr);
+	console_print(".");
+	console_putc('\n');
+	
+	__asm__ __volatile__("sti");
+	console_println("interrupts have been re-enabled.\n");
+	
 	asm volatile (
 		"mov %0, %%esp\n"
 		"jmp *%1\n"
@@ -29,47 +42,69 @@ int syscall_handler(uint32_t int_num, uint32_t error_code) {
 		  "=r"(arg4), "=r"(arg5)
 	);
 
-	switch (syscall_num) {
-		case SYSCALL_PUTCHAR:
-			console_putc((char)arg1);
-			return 1;
+	return handle_syscall(syscall_num, arg1, arg2, arg3, arg4, arg5);
+}
+
+
+int handle_syscall(uint32_t num, uint32_t arg1, uint32_t arg2, uint32_t arg3, uint32_t arg4, uint32_t arg5) {
+	switch (num) {
 		case SYSCALL_EXIT:
 			console_print("process exited with code ");
 			console_print_dec(arg1);
 			console_print(".");
 			console_putc('\n');
-
-			console_println("---------------------------------------------");
-			
-			console_println("Welcome back from userland!\n");
-			
-			console_print("Kernel ESP will be restored, value was 0x");
-			console_print_hex(kernel_ctx.esp);
-			console_print(".");
-			console_putc('\n');
-
-			console_print("Return address is 0x");
-			console_print_hex((uint32_t)kernel_ctx.ret_addr);
-			console_print(".");
-			console_putc('\n');
-
-			__asm__ __volatile__("sti");
-			console_println("Interrupts have been re-enabled.\n");
 			syscall_exit_trampoline(kernel_ctx.esp, kernel_ctx.ret_addr);
-					
-			break;
+			__builtin_unreachable();
+
+		case SYSCALL_PUTCHAR:
+			console_putc((char)arg1);
+			return 1;
+
+		case SYSCALL_GETCHAR:
+			__asm__ __volatile__("sti");
+			char c = read_key();
+			return (int)c;
+		
+		case SYSCALL_SLEEP:
+			__asm__ __volatile__("sti");
+			sleep((int)arg1);
+			return 1;
+		
+		case SYSCALL_UPTIME:
+			uint32_t ticks = uptime_seconds();
+			return (int)ticks;
+		
+		case SYSCALL_INPUT:
+			__asm__ __volatile__("sti");
+			char *buf = (char *)arg1;
+			uint32_t max_len = arg2;
+			console_input(buf, max_len);
+			return 1;
+		
+		case SYSCALL_CLEAR:
+			console_clear();
+			return 1;
+		
+		
+
+		default:
+			console_set_background_color(0xFF0000);
+			console_set_color(0xFFFFFF);
+			console_putc('\n');
+			console_putc('\n');
+			console_print("Sorry, this program was terminated by the kernel.\n    unimplemented trap (");
+			console_print_dec(num);
+			console_println(")");
+			console_print("roses are red,\nviolets are blue,\numimplemented trap!\nguess that one's on you.");
+			console_set_background_color(0x000000);
+			console_putc('\n');
+			console_putc('\n');
+
+			syscall_exit_trampoline(kernel_ctx.esp, kernel_ctx.ret_addr);
+			__builtin_unreachable();
 	}
-	
-	console_set_background_color(0xFF0000);
-	console_set_color(0xFFFFFF);
-	console_putc('\n');
-	console_print("unknown syscall (");
-	console_print_dec(syscall_num);
-	console_print(")");
-	console_set_background_color(0x000000);
-	console_putc('\n');
-	return -1;
 }
+
 
 int syscall(int num, int a, int b, int c, int d, int e) {
 	int ret;
--- kernel/sys/syscall.h
+++ kernel/sys/syscall.h
@@ -3,7 +3,13 @@
 // syscall numbers
 #define SYSCALL_EXIT 0
 #define SYSCALL_PUTCHAR 1
-// add more as needed
+#define SYSCALL_GETCHAR 2
+#define SYSCALL_SLEEP 3
+#define SYSCALL_UPTIME 4
+#define SYSCALL_INPUT 5
+#define SYSCALL_CLEAR 6
+
 
 int syscall_handler(uint32_t int_num, uint32_t error_code);
+int handle_syscall(uint32_t num, uint32_t arg1, uint32_t arg2, uint32_t arg3, uint32_t arg4, uint32_t arg5);
 int syscall(int num, int a, int b, int c, int d, int e);
